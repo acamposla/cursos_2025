d7ca58e1b2e280aeb5acf323e5d925f9bce503bf
Alejandro Campos <alejandro.campos@imprexeurope.com>
2025-01-05 17:00:11 +0100

    Actualización de registros de Timewarrior

---
+++ Codewars_Exercises/8_kyu/8_kyu-Reversed_Strings.py
@@ -0,0 +1,17 @@
+
+"""
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+StringsFundamentals
+"""
+
+
+string = "world"
+
+def solution(string):
+    return string[::-1]
+
+
+
