ff3ff37356adb460974fa7fd51f38d58568a4f94
Alejandro Campos <alejandro.campos@imprexeurope.com>
2025-02-02 20:13:09 +0100

    Actualización de registros de Timewarrior

---
+++ RealPython-LearningPath-Data_Science_With_Python_Core_Skills/4_Working_With_JSON_in_Python/.ipynb_checkpoints/thony-checkpoint.py
@@ -0,0 +1,19 @@
+import json
+
+file_path = "saved_todos.json"
+
+with open(file_path, "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+todos = json.loads(data)
+
+
+todos_by_user = {}
+
+
+for todo in todos:
+    if todo["completed"]:
+        todos_by_user[todo["userId"]] = todos_by_user.get(todo["userId"], 0) +1)
+    
+ 
+    
